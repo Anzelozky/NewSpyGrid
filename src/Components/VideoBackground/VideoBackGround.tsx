ba39e788e624c1774b699e849c2de652f76da4fd
Jonathan Lopez <crack.path86@gmail.com>
2023-03-02 01:01:54 +0000

    Remove unnecesary Title property

--- src/Components/VideoBackground/VideoBackGround.tsx
+++ src/Components/VideoBackground/VideoBackGround.tsx
@@ -2,15 +2,13 @@ import "./VideoBackGround.css";
 import BackgroundVideo from "../../assets/BackgroundVideo.mp4";
 
 type VideoBackgroundProps = {
-  title?: string;
   children?: JSX.Element;
 };
 
-const VideoBackground = ({ title, children }: VideoBackgroundProps) => {
+const VideoBackground = ({ children }: VideoBackgroundProps) => {
   return (
     <div className="videobackground">
       <video src={BackgroundVideo} autoPlay loop muted />
-      <p>{title}</p>
       {children}
     </div>
   );
