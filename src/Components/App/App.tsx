f242e1dcf7fddfbfb746422a7cf647e00dd70ca5
Jonathan Lopez <crack.path86@gmail.com>
2023-03-02 01:51:04 +0000

    Added profileGrid component

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,10 +1,21 @@
-import VideoBackground from "../VideoBackground/VideoBackGround";
 import "./App.css";
 
+import VideoBackground from "../VideoBackground/VideoBackGround";
+import ProfileGrid from "../ProfileGrid/ProfileGrid";
+
+import { useAgent } from "../../Hooks/useAgent";
+
 export default function App() {
-  return (
-    <div className="App">
-      <VideoBackground title=""></VideoBackground>
-    </div>
-  );
+  const [agent, isLoading, hasError] = useAgent();
+  console.log(agent, isLoading, hasError);
+
+  if (!isLoading) {
+    return (
+      <div className="App">
+        <VideoBackground>
+          <ProfileGrid agent={agent}></ProfileGrid>
+        </VideoBackground>
+      </div>
+    );
+  }
 }
